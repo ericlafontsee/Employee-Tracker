6ac920a69aa3771613cec916bc06436ed1080c8d
Eric LaFontsee <elafontsee@gmail.com>
2020-09-21 07:22:12 -0500

    Set up database schema and index.js connection

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var { prompt } = require("inquirer");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
 
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId + "\n");
+    init();
+})
